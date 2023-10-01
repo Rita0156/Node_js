16a29653963cad6477d9f19650c4acb54f28f4d6
Rita0156 <ritachaudhari006@gmail.com>
2023-10-01 21:17:59 +0530

    det

--- day2/index.js
+++ day2/index.js
@@ -8,6 +8,9 @@ const server=http.createServer((req,res)=>{
     else if(req.url==="/public"){
         return res.end("Welcome to Public page")
     }
+    else if(req.url==="/public/details"){
+        return res.end("Welcome to Public detailes",[1,2,3,4,5,6,7])
+    }
 })
 
 server.listen(8080,()=>{
