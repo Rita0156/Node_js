da218721cd11ca16439d2ee42c3ed28af2ed8694
Rita0156 <ritachaudhari006@gmail.com>
2023-10-02 18:58:41 +0530

    json

--- day_3/index.js
+++ day_3/index.js
@@ -1,2 +1,29 @@
 
-const app=require("express")
+const express=require("express");
+
+const app=express();
+
+app.get("/",(req,res)=>{
+    res.send("Welcome to HomePage")
+})
+app.post("/",(req,res)=>{
+    res.send("Welcome to HomePage")
+})
+app.put("/",(req,res)=>{
+    res.send("Welcome to HomePage")
+})
+app.patch("/",(req,res)=>{
+    res.send("Welcome to HomePage")
+})
+app.delete("/",(req,res)=>{
+    res.send("Welcome to HomePage")
+})
+
+app.listen(8080,()=>{
+    console.log("server is started");
+})
+
+
+
+
+
