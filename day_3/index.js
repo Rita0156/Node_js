f8808ea6f73f18b4a26f3fa3cede3913df4a591e
Rita0156 <ritachaudhari006@gmail.com>
2023-10-03 21:10:02 +0530

    post

--- day_3/index.js
+++ day_3/index.js
@@ -1,21 +1,34 @@
 
 const express=require("express");
-
+const fs=require("fs")
 const app=express();
 
 app.get("/",(req,res)=>{
-    res.send("Welcome to HomePage")
+  const  data=fs.readFileSync("./db,json","utf-8")
+    res.send(data)
 })
 app.post("/",(req,res)=>{
-    res.send("Welcome to HomePage")
+    
+
+   const data=fs.readFileSync("./db,json","utf-8")
+
+   const payload=req.body
+    const strigFyData=JSON.parse(payload)
+  const newData=[...data,strigFyData]
+    res.send(newData)
 })
 app.put("/",(req,res)=>{
+    
+    const data=fs.readFileSync("./db,json","utf-8")
+    
     res.send("Welcome to HomePage")
 })
 app.patch("/",(req,res)=>{
+    const data=fs.readFileSync("./db,json","utf-8")
     res.send("Welcome to HomePage")
 })
 app.delete("/",(req,res)=>{
+    const data=fs.readFileSync("./db,json","utf-8")
     res.send("Welcome to HomePage")
 })
 
