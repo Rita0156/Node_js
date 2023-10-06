c36016b26be9649c0146889b21bf4d886656905c
Rita0156 <ritachaudhari006@gmail.com>
2023-10-06 21:42:09 +0530

    post

--- day_3/index.js
+++ day_3/index.js
@@ -4,37 +4,46 @@ const fs=require("fs")
 const app=express();
 
 app.get("/",(req,res)=>{
-  const  data=fs.readFileSync("./db,json","utf-8")
+    res.send("Welcome to home page")
+})
+
+app.get("/about",(req,res)=>{
+  const  data=fs.readFileSync("./db.json","utf-8")
     res.send(data)
 })
-app.post("/",(req,res)=>{
+app.post("/about",(req,res)=>{
     
 
-   const data=fs.readFileSync("./db,json","utf-8")
+   const data=fs.readFileSync("./db.json","utf-8")
 
    const payload=req.body
     const strigFyData=JSON.parse(payload)
   const newData=[...data,strigFyData]
     res.send(newData)
+    console.log("data added")
 })
-app.put("/",(req,res)=>{
+app.put("/about",(req,res)=>{
     
-    const data=fs.readFileSync("./db,json","utf-8")
+    const data=fs.readFileSync("./db.json","utf-8")
     
     res.send("Welcome to HomePage")
 })
-app.patch("/",(req,res)=>{
-    const data=fs.readFileSync("./db,json","utf-8")
+app.patch("/about",(req,res)=>{
+    const data=fs.readFileSync("./db.json","utf-8")
     res.send("Welcome to HomePage")
 })
-app.delete("/",(req,res)=>{
-    const data=fs.readFileSync("./db,json","utf-8")
+app.delete("/about",(req,res)=>{
+    const data=fs.readFileSync("./db.json","utf-8")
     res.send("Welcome to HomePage")
 })
 
 app.listen(8080,()=>{
     console.log("server is started");
 })
+
+// post data completed.
+//get method completed.
+// patch data 
 
 
 
