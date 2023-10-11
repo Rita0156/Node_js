9011e12b34cf42fc9ffad5bc0e2fd5a1f8a63fc6
Rita0156 <ritachaudhari006@gmail.com>
2023-10-11 22:43:50 +0530

    pat

--- day_3/index.js
+++ day_3/index.js
@@ -17,22 +17,33 @@ app.get("/about",(req,res)=>{
 })
 app.post("/about",(req,res)=>{
     
+ 
+  let payload=req.body;
+  let add=JSON.parse(payload)
+  
+  const  dataF=fs.readFileSync("./db.json","utf-8")
+  const newData=[...dataF,payload]
+  //const strData=JSON.parse(dataF)
+  //const posts=strData.post
+  //console.log(strData.products)
+  console.log(newData,"item added in cart")
+    res.send(JSON.stringify(newData.products))
 
    const data=fs.readFileSync("./db.json","utf-8")
 
    
 })
-app.put("/about",(req,res)=>{
+app.put("/about:id",(req,res)=>{
     
     const data=fs.readFileSync("./db.json","utf-8")
     
     res.send("Welcome to HomePage")
 })
-app.patch("/about",(req,res)=>{
+app.patch("/about:id",(req,res)=>{
     const data=fs.readFileSync("./db.json","utf-8")
     res.send("Welcome to HomePage")
 })
-app.delete("/about",(req,res)=>{
+app.delete("/about:id",(req,res)=>{
     const data=fs.readFileSync("./db.json","utf-8")
     res.send("Welcome to HomePage")
 })
