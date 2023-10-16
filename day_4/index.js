2d9965e3cf167fbe7d9f1fd5f666ab08bfdada33
Rita0156 <ritachaudhari006@gmail.com>
2023-10-16 23:18:20 +0530

    no

--- day_4/index.js
+++ day_4/index.js
@@ -7,32 +7,36 @@ const app=express();
 app.use(express.json())
 
 app.get("/",(req,res)=>{
-    res.send("Welcome to Homepage")
+    res.send("Welcome to Homepage");
 })
 app.get("/mark",(res,req)=>{
-    const alldata=ModelMark.marks
-    res.send(alldata)
+    const alldata=ModelMark.marks;
+    res.send(alldata);
 })
 app.post("/addmark",(res,req)=>{
-    const payload ={mark1,mark2,remark}=req.body()
+    const payload ={mark1,mark2,remark}=req.body();
     const newData=ModelMark.payload.save();
-    res.send(newData)
+    res.send(newData);
 
 
 })
 app.delete(`/mark:${id}`,(res,req)=>{
-    ModelMark.delete({id:id})
-    res.send("item deleted")
+    ModelMark.delete({id:id});
+    res.send("item deleted");
 })
 
 app.listen(7000,async()=>{
     try{
-      const data=   await connection()
-      console.log("connected successfully")
+      const data=   await connection();
+      console.log("connected successfully");
     }
     catch{
-        console.log("failed to connect")
+        console.log("failed to connect");
     }
-    console.log("running server is 7000 port")
+    console.log("running server is 7000 port");
 })
+//put req is pending
+//patch req is pending
+// mvc is pending
+//work in progress
 
