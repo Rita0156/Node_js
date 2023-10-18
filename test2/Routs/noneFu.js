f5a9daa7d05bc6a480fcfe80fbf82b4438bbe04a
Rita0156 <ritachaudhari006@gmail.com>
2023-10-18 11:58:06 +0530

    not

--- test2/Routs/noneFu.js
+++ test2/Routs/noneFu.js
@@ -1,3 +1,16 @@
 
 
 const {NoteModel}=require("./Pages/note.js")
+
+
+const {Router}=require("Router")
+const NotenHander=Router()
+ NoteHander.post("/login",(req,res)=>{
+   const {email,password}=req.body()
+   if(email&&password){
+    res.send("login successfull")
+   }
+   else{
+    res.send("email or password is incorrect")
+   }
+})
