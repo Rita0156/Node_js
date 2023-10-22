7bf3cb0fd2b92985f13000dd9b5185cdf883d560
Rita0156 <ritachaudhari006@gmail.com>
2023-10-22 20:11:26 +0530

    post

--- auth/Routs/singUpCon.js
+++ auth/Routs/singUpCon.js
@@ -2,3 +2,10 @@
 
 const {SingUpModel}=require("../Models/singup")
 const mongoose=require("mongoose")
+const {Router}=require("express")
+const Singupcontroller=Router()
+
+SingUpModel.post("/adduser",async(req,res)=>{
+        const payload=req.body()
+        
+})
