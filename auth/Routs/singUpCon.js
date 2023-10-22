8d11d175398f6b836eadc19cb9e7a00a448b31cf
Rita0156 <ritachaudhari006@gmail.com>
2023-10-22 21:23:41 +0530

    asd

--- auth/Routs/singUpCon.js
+++ auth/Routs/singUpCon.js
@@ -5,7 +5,17 @@ const mongoose=require("mongoose")
 const {Router}=require("express")
 const Singupcontroller=Router()
 
-SingUpModel.post("/adduser",async(req,res)=>{
+Singupcontroller.post("/adduser",async(req,res)=>{
         const payload=req.body()
-        
+        const user=new SingUpModel(payload)
+        await user.save()
+        res.send("Sing Up Successfull")
 })
+
+Singupcontroller.post("/user",async(req,res)=>{
+    const data=req.body()
+    const valid= await SingUpModel.findOne(data)
+    const token=jwt.sing({})
+})
+
+module.exports={Singupcontroller}
