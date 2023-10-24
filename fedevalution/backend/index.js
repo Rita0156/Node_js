9b5a071824f05721151feff0437385f1cb151498
Rita0156 <ritachaudhari006@gmail.com>
2023-10-24 12:46:20 +0530

    back

--- fedevalution/backend/index.js
+++ fedevalution/backend/index.js
@@ -1,11 +1,19 @@
 
 const express=require("express")
 const {connection}=require("./confing/db.js")
+const {SingupModel}=require("./Model/singupModel.js")
 
 const app=express()
 app.use(express.json())
 app.get("/",(req,res)=>{
     res.send("Welcome to homepage....")
+})
+
+app.post("/singup",async(req,res)=>{
+    const payload=req.body()
+    const data=new SingupModel(payload)
+     await data.save()
+     res.send("Sing up successfull")
 })
 
 app.listen(7500,async(req,res)=>{
